2d4bfa7972139cf13f0fd9b34bb2b91529028d5d
An Le Thanh <anlethanh@Ans-MacBook-Pro.local>
2021-02-12 16:31:08 +0800

    added inner cart counter

--- src/flowers/FlowerItem.tsx
+++ src/flowers/FlowerItem.tsx
@@ -1,13 +1,33 @@
+import { useState } from 'react';
+import { FaPlus, FaMinus } from 'react-icons/fa';
 import { Item } from '../models/CartItem';
 import './flower-item.css';
 
 function FlowerItem({ details }: FlowerItemProps) {
   const { displayName, imageUrl, price } = details;
+  const [count, setCount] = useState(0);
+
+  const addItem = () => {
+    setCount(count + 1);
+  };
+
+  const removeItem = () => {
+    if (count > 0) setCount(count - 1);
+  };
+
   return (
     <article className="flower-item-wrapper">
       <img src={imageUrl} alt="Girl in a jacket" />
       <h4>{displayName}</h4>
       <p>{price} SGD</p>
+
+      <div className="flower-item-counter">
+        <FaMinus onClick={removeItem}></FaMinus>
+        <span className="spacer"></span>
+        {count}
+        <span className="spacer"></span>
+        <FaPlus onClick={addItem}></FaPlus>
+      </div>
     </article>
   );
 }
